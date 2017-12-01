9cee0505b0783d057a425596f8e74a9e19e9ad5b
gaul <fallin1008@gmail.com>
2017-12-01 14:35:51 +0900

    Changed the HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from .import views
+
+urlpatterns = [
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^mapspage.html$', views.mapspage, name='mapspage'),
+    url(r'^aboutus.html$', views.aboutus, name='aboutus'),
+    url(r'^contactus.html$', views.contactus, name='contactus'),
+]
